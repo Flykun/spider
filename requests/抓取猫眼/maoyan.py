09d204e5c973b88ecf86d3f8862ae34185048ee3
Flykun <1689890728@qq.com>
2020-05-06 19:20:47 +0800

    '分页爬取,本节代码'

--- requests/抓取猫眼/maoyan.py
+++ requests/抓取猫眼/maoyan.py
@@ -44,15 +44,18 @@ def write_to_file(content):
         f.write(json.dumps(content, ensure_ascii=False) + '\n')
 
 
-def main():
+def main(offset):
     # 主函数
-    url = 'http://maoyan.com/board/4'
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
     html = get_one_page(url)
     for item in parse_one_page(html):
+        print(item)
         write_to_file(item)
 
 
-main()
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i * 10)
 
 """
 排名信息: '<dd>.*?board-index.*?>(\d+)</i>'
