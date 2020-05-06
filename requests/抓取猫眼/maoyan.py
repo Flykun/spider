2c4a423d48720baa934fb4f468682bffda4d0db4
Flykun <1689890728@qq.com>
2020-05-06 19:03:28 +0800

    代码整合

---
+++ requests/抓取猫眼/maoyan.py
@@ -0,0 +1,64 @@
+import re
+import json
+import requests
+
+"""
+抓取首页
+"""
+
+
+def get_one_page(url):
+    # 抓取页面
+    headers = {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+def parse_one_page(html):
+    # 解析页面
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+        + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+        + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score': item[5].strip() + item[6].strip()
+        }
+
+
+def write_to_file(content):
+    # 写入文件
+    with open('result.txt', 'a', encoding='utf8') as f:
+        print(type(json.dumps(content)))
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+def main():
+    # 主函数
+    url = 'http://maoyan.com/board/4'
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        write_to_file(item)
+
+
+main()
+
+"""
+排名信息: '<dd>.*?board-index.*?>(\d+)</i>'
+图片链接: 
+电影名: 
+主演:
+发布时间: 
+评分: 
+"""
